a179d52f97197625964e70773f5dd7b008697f02
Raul <rmartinez2416@gmail.com>
2022-05-07 13:45:29 -0700

    Created the computerPlay function.

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-console.log('hello')
+let compChoice = ''
+
+function computerPlay(){
+    choices = ["Rock", "Paper", "Scissors"]
+    let random = choices[Math.floor(Math.random() * 3)];
+    return compChoice = random;   
+}
+
+
+
+
